eb03a83320001c7208868b158802b82a49b6aed5
Gabriel Alcantara <gabrielalcantaratp@gmail.com>
2019-06-29 12:09:59 -0300

    criação de model e controller de cadastro de aut

---
+++ app/Http/Controllers/AutomovelCadController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AutomovelCad;
+use Illuminate\Http\Request;
+
+class AutomovelCadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AutomovelCad  $automovelCad
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AutomovelCad $automovelCad)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AutomovelCad  $automovelCad
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AutomovelCad $automovelCad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AutomovelCad  $automovelCad
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AutomovelCad $automovelCad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AutomovelCad  $automovelCad
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AutomovelCad $automovelCad)
+    {
+        //
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/home') }}">
             <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-laugh-wink"></i>
             </div>
@@ -36,7 +36,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-            <a class="nav-link" href="dashboard.html">
+            <a class="nav-link" href="{{ url('/home') }}">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Dashboard</span></a>
             </li>
