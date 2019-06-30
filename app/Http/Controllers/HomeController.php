6473635fd53e0d8e8e7c40beb593c8b79343e87e
Gabriel Alcantara <gabrielalcantaratp@gmail.com>
2019-06-30 09:31:22 -0300

    select com intervalo mensal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,9 +27,10 @@ class HomeController extends Controller
         // $client = DB::table('clients')->count('id');
         $client = DB::table('clients')->select(DB::raw('count(id) as id_client'))->get();
         $service = DB::table('services')->select(DB::raw('count(id) as id_service'))->get();
-        $wash = DB::table('washes')->join('services', 'wash.id_service', '=', 'services.id')
-                                   ->select(DB::raw('services.id, services.name, sum(services.price)'))
-                                   ->where('washes.created_at > (NOW() - INTERVAL 30 DAY)');
+        $wash = DB::table('washes')->join('services', 'washes.id_service', '=', 'services.id')
+                                   ->select(DB::raw('sum(services.price) as price'))
+                                   ->where('washes.created_at', '>', DB::raw('(NOW() - INTERVAL 30 DAY)'))
+                                   ->get();
         return view('home', compact('client', 'service', 'wash'));
     }
 }
