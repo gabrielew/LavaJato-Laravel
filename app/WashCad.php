9bc3595c02980461958e9893f7d30a1ca4658d21
Gabriel Alcantara <gabrielalcantaratp@gmail.com>
2019-06-30 08:30:51 -0300

    fillable

--- app/WashCad.php
+++ app/WashCad.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class WashCad extends Model
 {
-    //
+    protected $fillable = ['id_client', 'id_car', 'id_service'];
+    protected $guarded = ['id', 'created_at'];
+    protected $table = 'washes';
 }
