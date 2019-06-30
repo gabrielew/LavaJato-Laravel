57b1b704554c0602d775869a8a4c88dd3161958c
Gabriel Alcantara <gabrielalcantaratp@gmail.com>
2019-06-30 08:35:02 -0300

    populando controller wash

--- app/Http/Controllers/WashCadController.php
+++ app/Http/Controllers/WashCadController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use App\WashCad;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class WashCadController extends Controller
 {
+    protected $redirectTo = '/home';
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -17,14 +25,28 @@ class WashCadController extends Controller
         //
     }
 
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'cpf' => ['required', 'string', 'max:20', 'unique:clients'],
+            'birthdate' => ['required', 'date', 'max:255'],
+        ]);
+    }
+
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(array $data)
     {
-        //
+        return User::create([            
+            'id_client' => $data['id_client'],
+            'id_car' => $data['id_car'],
+            'id_service' => $data['id_service'],
+        ]);
+        
     }
 
     /**
@@ -35,7 +57,12 @@ class WashCadController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $wash = new WashCad;
+        $wash->id_client = $request->id_client;
+        $wash->id_car = $request->id_car;
+        $wash->id_service = $request->id_service;
+        $wash->save();
+        return redirect()->route('home')->with('message', 'Lavagem cadastrada com sucesso!');
     }
 
     /**
@@ -57,7 +84,8 @@ class WashCadController extends Controller
      */
     public function edit(WashCad $washCad)
     {
-        //
+        $wash = WashCad::findOrFail($id);
+        return view('home', compact('wash'));
     }
 
     /**
@@ -69,7 +97,12 @@ class WashCadController extends Controller
      */
     public function update(Request $request, WashCad $washCad)
     {
-        //
+        $wash = WashCad::findOrFail($id);
+        $wash->id_client = $request->id_client;
+        $wash->id_car = $request->id_car;
+        $wash->id_service = $request->id_service;
+        $car->save();
+        return redirect()->route('home')->with('message', "Lavagem Atualizada com sucesso!");
     }
 
     /**
@@ -80,6 +113,8 @@ class WashCadController extends Controller
      */
     public function destroy(WashCad $washCad)
     {
-        //
+        $wash = WashCad::findOrFail($id);
+        $wash->delete();
+        return redirect()->route('home')->with('alert-success', 'Lavagem removida da base de dados!');
     }
 }
