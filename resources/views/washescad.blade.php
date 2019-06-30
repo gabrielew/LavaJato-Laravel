5e6dd2272df2440ff3e2e66a8b4680d0c7fd3b74
Gabriel Alcantara <gabrielalcantaratp@gmail.com>
2019-06-30 20:12:22 -0300

    Imagem na view de cadastro de lavagem

--- resources/views/washescad.blade.php
+++ resources/views/washescad.blade.php
@@ -22,7 +22,9 @@
                     <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image">imagem aq</div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image">
+                                <img style="height: 100%; width: 114%;" src="{{ asset('img/b1.JPG') }}" alt="">
+                            </div>
                                 <div class="col-lg-6">
                                     <div class="p-5">
                                         <div class="text-center">
